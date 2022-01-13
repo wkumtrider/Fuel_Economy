911187bb60c0d189494e2103d4fda19d1de3499f
sizemj <sizemj@gmail.com>
2022-01-12 21:17:47 -0500

    spelling corrections

--- Program.cs
+++ Program.cs
@@ -30,8 +30,8 @@ namespace FuelEconomy
             menu.Append("\n");
             menu.Append("\n");
             menu.Append("\nWelcome to the Fuel Economy Database.");
-            menu.Append("\nThis database allows you to look up the fuel economy of vehciles from 2005 to 2017.");
-            menu.Append("\nTYou can also enter a fuel economy and return a list of vehicles that meet the request!");
+            menu.Append("\nThis database allows you to look up the fuel economy of vehicles from 2005 to 2017.");
+            menu.Append("\nYou can also enter a fuel economy and return a list of vehicles that meet the request!");
             menu.Append("\n----------------------------");
             menu.Append("\nTo search by vehicle, enter 1.");
             menu.Append("\nTo search by fuel economy, press 2");
